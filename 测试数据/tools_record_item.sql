64245e7abc350904738944261b2c3039a723649c
曹林伟 <caolinwei@ebidding.com.cn>
2016-03-10 18:02:44 +0800

    sql

---
+++ 测试数据/tools_record_item.sql
@@ -0,0 +1,3 @@
+INSERT INTO `tools_record_item` VALUES (1, '111', 2, '111111');
+INSERT INTO `tools_record_item` VALUES (2, '222', 1, '222222');
+INSERT INTO `tools_record_item` VALUES (3, '333', 3, '333333');
